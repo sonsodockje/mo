13e3a9c080662f650a37c944715d175aeb08e897
sonsodockje <hanaroo0115@naver.com>
2024-03-06 17:12:54 +0900

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// https://firebase.google.com/docs/web/setup#available-libraries
+// https://firebase.google.com/docs/reference/js/database.md?authuser=1&hl=ko#interfaces
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCn_aqgfUogtQ0i6ztjHdRn3PV9dH9D64",
+  authDomain: "miniitter.firebaseapp.com",
+  databaseURL: "https://miniitter-default-rtdb.firebaseio.com",
+  projectId: "miniitter",
+  storageBucket: "miniitter.appspot.com",
+  messagingSenderId: "536393511478",
+  appId: "1:536393511478:web:2ea68957d4200c51c19e97",
+  aa,
+};
+
+const app = initializeApp(firebaseConfig);
