7dbabd4bceda757574d3865082f1aa2aa4bc355f
sonsodockje <hanaroo0115@naver.com>
2024-03-07 18:41:28 +0900

    feat axios api 연결

---
+++ src/components/Info.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { searchMovieInfo } from "../api";
+
+export default function Info({ props }) {
+  const [data, setData] = useState([]);
+  const [engNm, setEngNm] = useState("");
+
+  const movieCd = useParams().id;
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const MovieInfo = await searchMovieInfo(movieCd);
+        setData(MovieInfo);
+        setEngNm(MovieInfo.movieNmEn);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {data.movieNm} and {engNm}
+    </div>
+  );
+}
