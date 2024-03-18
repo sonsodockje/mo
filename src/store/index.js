5898af1928d74f4537f107b8357b0bd8527fa58f
sonsodockje <hanaroo0115@naver.com>
2024-03-18 16:59:26 +0900

    update signup 코드 정리

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,3 @@
-import { initializeApp } from "firebase/app";
 import { create } from "zustand";
 
 export const useAuthStore = create((set) => ({
@@ -8,15 +7,11 @@ export const useAuthStore = create((set) => ({
   },
 }));
 
-//evn파일에 키값을 넣어두는게 좋다고해서.. 그렇게했읍니다..
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_APP_FIREBASE_DATABASE_UR,
-  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
-};
-
-export const app = initializeApp(firebaseConfig);
+export const useSignUpStore = create((set) => ({
+  email: "",
+  password: "",
+  name: "",
+  setEmail: (email) => set({ email: email }),
+  setPassword: (password) => set({ password: password }),
+  setName: (name) => set({ name: name }),
+}));
